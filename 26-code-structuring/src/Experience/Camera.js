0098e191295eb07249debb3ab32507df45fb3b41
marp98 <manuelroshardt@gmail.com>
2023-02-27 08:59:50 -0500

    project structure created

---
+++ 26-code-structuring/src/Experience/Camera.js
@@ -0,0 +1,6 @@
+export default class Camera {
+  constructor() {
+    this.experience = window.experience;
+    console.log("my camera");
+  }
+}
--- 26-code-structuring/src/script.js
+++ 26-code-structuring/src/script.js
@@ -1,3 +1,9 @@
+import "./style.css";
+
+import Experience from "./Experience/Experience";
+
+const experience = new Experience(document.querySelector("canvas.webgl"));
+
 // import * as THREE from 'three'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // import * as dat from 'lil-gui'
