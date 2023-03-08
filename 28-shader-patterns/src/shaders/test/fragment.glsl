e26e802f0fcdcc9aefa9934db162541f378bf312
marp98 <manuelroshardt@gmail.com>
2023-03-08 08:53:01 -0500

    drawing different patterns

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -1,4 +1,9 @@
+varying vec2 vUv;
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    float strength = mod(vUv.y * 10.0, 1.0);
+
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
+    // gl_FragColor = vec4(vUv, 1.0, 1.0);
 }
